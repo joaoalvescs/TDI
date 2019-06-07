08fcf0c806d031bee4c2674a585e33d7975117a6
Joao <joaoalves.cs@gmail.com>
2019-06-07 12:18:15 -0300

    Criação do arquivo Operacao.cs. Calcula as quatro operações matemáticas e o resto da divisão.

---
+++ C#/Operacao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, informe um número. ");
+            float numero = Console.Read();
+            Console.WriteLine("Por favor, informe um número. ");
+            float numero1 = Console.Read();
+            Console.WriteLine("Soma: " + soma(numero, numero1));
+            Console.WriteLine("Divisão: " + divisao(numero, numero1));
+            Console.WriteLine("Multiplicação: " + multiplicacao(numero, numero1));
+            Console.WriteLine("Subtração: " + subtracao(numero, numero1));
+            Console.Write("Resto: " + numero % numero1);
+        }
+
+        public static float soma(float numero, float numero1)
+        {
+            return numero + numero1;
+        }
+
+        public static float divisao(float numero, float numero1){
+            return numero / numero1;
+        }
+
+        public static float multiplicacao(float numero, float numero1)
+        {
+            return numero * numero1;
+        }
+
+        public static float subtracao(float numero, float numero1){
+            return numero - numero1;
+        }
+    }
+}
