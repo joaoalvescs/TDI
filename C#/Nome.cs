dbe4ab559e8e02a1b6f78315bde8416626f9eb94
Joao <joaoalves.cs@gmail.com>
2019-06-07 11:40:56 -0300

    Criação da pasta C#

---
+++ C#/Nome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            Console.WriteLine("Por favor, informe seu nome. ");
+            nome = Console.ReadLine();
+            Console.WriteLine(" O seu nome é " + nome);
+            Console.ReadKey();
+        }
+    }
+}
