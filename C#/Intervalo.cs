4585ec38b46b3ffc0808b6b9dddfdf195a4dc336
Joao <joaoalves.cs@gmail.com>
2019-06-07 12:36:03 -0300

    Criação do arquivo Intervalo.cs. Verifica o intervalo de um número entre 10 e 100.

---
+++ C#/Intervalo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, informe um número. ");
+            int numero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(intervalo(numero));
+        }
+
+        public static bool intervalo(int numero)
+        {
+            if (numero > 10 && numero < 100)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+        }
+    }
+}
