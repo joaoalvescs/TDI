cd11952aa6d1bb79654c8b1fb8a2623c093259ee
Joao <joaoalves.cs@gmail.com>
2019-07-05 12:07:58 -0300

    Exercícios que estavam no computador do laboratório.

---
+++ repos/TDI2/TDI2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDI2
+{
+    class Estruturas
+    {
+        static void Main()
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(8);
+            Console.WriteLine(numbers.Count);
+        }
+    }
+}
